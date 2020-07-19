0c45f2cc3d4cb7230b8bae325f315aaaccb58213
riyagulhare <56391758+riyagulhare@users.noreply.github.com>
2020-07-19 23:52:53 +0530

    Add files via upload

---
+++ app1.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    }); 
+    $('#autoWidth1').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth1').removeClass('cS-hidden');
+        } 
+    });
+    $('#autoWidth2').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth2').removeClass('cS-hidden');
+        } 
+    }); 
+    $('#autoWidth3').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth3').removeClass('cS-hidden');
+        } 
+    }); 
+});
