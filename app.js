f19f4195d951a9429c74f91d64a99df0da273ce2
riyagulhare <56391758+riyagulhare@users.noreply.github.com>
2020-07-19 12:08:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,5 +12,12 @@ $(document).ready(function() {
         onSliderLoad: function() {
             $('#autoWidth1').removeClass('cS-hidden');
         } 
-    });  
+    });
+    $('#autoWidth2').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth2').removeClass('cS-hidden');
+        } 
+    });
   });
